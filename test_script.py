9cb656d02f2727114671c78719d1040ea21cbf50
Dominic Waithe <dominic_waithe@hotmail.com>
2015-01-15 17:15:02 +0000

    bug fixes

--- test_script.py
+++ test_script.py
@@ -2,31 +2,27 @@
 
 from correlation_objects import picoObject
 
+
 class ParameterClass():
-    def __init__(self):
-        
-        #Where the data is stored.
-        self.data = []
-        self.objectRef =[]
-        self.subObjectRef =[]
-        self.colors = ['blue','green','red','cyan','magenta','yellow','black']
-        self.numOfLoaded = 0
-        self.NcascStart = 0
-        self.NcascEnd = 25
-        self.Nsub = 6
-        self.winInt = 10
-        self.photonCountBin = 25
-
-
-
-
-
+	"""Stores parameters for correlation """
+	def __init__(self):
+		
+		#Where the data is stored.
+		self.data = []
+		self.objectRef =[]
+		self.subObjectRef =[]
+		self.colors = ['blue','green','red','cyan','magenta','yellow','black']
+		self.numOfLoaded = 0
+		self.NcascStart = 0
+		self.NcascEnd = 25
+		self.Nsub = 6
+		self.winInt = 10
+		self.photonCountBin = 25
 
 par_obj = ParameterClass()
 
+#Runs correlation on the path/to/filename.
+pt3file = picoObject("topfluorPE_2_1_1_1.pt3", par_obj, None)
 
-
-picoObject("topfluorPE_2_1_1_1.pt3", par_obj, None)
-
-print 'tau: '+self.autotime
-print 'normalised correlation'+self.autoNorm 
+print 'tau: ', pt3file.autotime
+print 'normalised correlation', pt3file.autoNorm 
