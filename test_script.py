7e3be668c944902006677789f5d31ffe111ce720
Dominic Waithe <dominic_waithe@hotmail.com>
2015-01-15 17:10:44 +0000

    Added content to test_script.py

--- test_script.py
+++ test_script.py
@@ -1,3 +1,32 @@
 #Tests the main functionality of the correlation without the interface.
 
 from correlation_objects import picoObject
+
+class ParameterClass():
+    def __init__(self):
+        
+        #Where the data is stored.
+        self.data = []
+        self.objectRef =[]
+        self.subObjectRef =[]
+        self.colors = ['blue','green','red','cyan','magenta','yellow','black']
+        self.numOfLoaded = 0
+        self.NcascStart = 0
+        self.NcascEnd = 25
+        self.Nsub = 6
+        self.winInt = 10
+        self.photonCountBin = 25
+
+
+
+
+
+
+par_obj = ParameterClass()
+
+
+
+picoObject("topfluorPE_2_1_1_1.pt3", par_obj, None)
+
+print 'tau: '+self.autotime
+print 'normalised correlation'+self.autoNorm 
