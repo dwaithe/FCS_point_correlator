c745d87ed8c8c9f689d03405d0335425efc678c0
Dominic Waithe <dominic_waithe@hotmail.com>
2015-05-11 12:07:16 +0100

    added fib.pyx and setup.py so people can compile with distutils

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import os
+import platform
+import numpy
+
+setup(ext_modules = cythonize("fib.pyz"),include_dirs=[numpy.get_include()])
