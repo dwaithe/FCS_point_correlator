1335092f01f8c5d1a3353f6d2551d07fdb53a5ae
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 18:37:13 +0100

    structural changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='focuspoint',
 	version='0.1',
 	author='Dominic Waithe',
-	#install_requires=['numpy','scipy','lmfit'],
+	install_requires=['numpy','scipy','lmfit','matplotlib'],
 	include_package_data=True,
 	packages = ['focuspoint'],
 	entry_points={
