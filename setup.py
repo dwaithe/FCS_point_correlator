824dd87b3ad42586c44eccdf9687ad4d6fdb6c09
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 11:23:54 +0100

    fixing setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import numpy
 setup(ext_modules = cythonize("fib.pyz"),
 	include_dirs=[numpy.get_include()],
 	author='Dominic Waithe',
-	install_requires=['lmfit']
+	install_requires=['numpy','scipy','lmfit']
 
 
 	)
