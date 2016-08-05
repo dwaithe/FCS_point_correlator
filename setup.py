a7f4cbe38e4fd8ee676dc529daf97e187f5b1279
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 13:53:08 +0100

    fixing setup

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from distutils.core import setup
-from Cython.Build import cythonize
-import os
-import platform
-import numpy
 
-setup(ext_modules = cythonize("fib.pyz"),
-	include_dirs=[numpy.get_include()],
+
+setup(
+	
 	author='Dominic Waithe',
-	install_requires=['Cython','Cython.Build','numpy','scipy','lmfit']
+	install_requires=['numpy','scipy','lmfit']
 
 
 	)
