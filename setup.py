28e6bee059580696b5309317c94724bf7fff5bb2
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 20:48:24 +0100

    pip install

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='focuspoint',
 	install_requires=['numpy','scipy','lmfit','pypng','matplotlib','cython'],
 	include_package_data=True,
 	packages = ['focuspoint'],
+	ext_modules=[Extension('focuspoint.fib4', ['focuspoint.fib4/focuspoint.c'])],
 	package_data={
         'focuspoint': ['fib4.pyx'],
     },
@@ -17,16 +18,3 @@ setup(name='focuspoint',
 
 
 	)
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-import numpy
-
-setup(
-	packages = ['focuspoint'],
-	package_data={
-        'focuspoint': ['fib4.pyx'],
-    },
-    include_dirs=[numpy.get_include()] ,cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("fib4", ["fib4.pyx"])]
-)
