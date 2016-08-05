869d11640711aa3ecc168fe728a427ff4d6f21e6
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 14:58:19 +0100

    fixing setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='fcspointcorrelator',
 	version='0.1',
 	author='Dominic Waithe',
 	install_requires=['numpy','scipy','lmfit'],
+	include_package_data=True,
 	packages = find_packages(),
 	entry_points={
         'console_scripts': [
