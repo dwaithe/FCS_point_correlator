e3c29b7ffb3343d8abda08389007f007f537f9af
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-05 19:27:50 +0100

    structural changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='focuspoint',
 	version='0.1',
 	author='Dominic Waithe',
-	install_requires=['numpy','scipy','lmfit','matplotlib'],
+	install_requires=['numpy','scipy','lmfit','pypng','matplotlib','cython'],
 	include_package_data=True,
 	packages = ['focuspoint'],
 	entry_points={
@@ -11,5 +11,15 @@ setup(name='focuspoint',
             'sample=sample:main',
         ],
     },
+    ext_modules=['fib4.pyx']
 
 	)
+from distutils.command.sdist import sdist as _sdist
+
+class sdist(_sdist):
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are up-to-date
+        from Cython.Build import cythonize
+        cythonize(['fib4.pyx'])
+        _sdist.run(self)
+cmdclass['sdist'] = sdist
