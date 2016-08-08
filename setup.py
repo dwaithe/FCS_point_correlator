7022cfaa1f9688c4a990d6a746252316102453cf
Dominic Waithe <dominic_waithe@hotmail.com>
2016-08-08 10:14:56 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages, Extension
-
+import numpy
 setup(name='focuspoint',
 	version='0.1',
 	author='Dominic Waithe',
-	install_requires=['numpy','scipy','lmfit','pypng','matplotlib','cython'],
+	install_requires=['numpy','scipy','lmfit','matplotlib','cython'],
 	include_package_data=True,
-	
+	include_dirs=[numpy.get_include()],
+	ext_modules=[Extension('focuspoint.fib4', ['focuspoint/fib4.c'])]
 	packages = ['focuspoint'],
 	
 	package_data={
@@ -20,11 +21,4 @@ setup(name='focuspoint',
 
 	)
 
-import numpy
 
-setup(
-	name='cythonmaxima',
-	version='0.1',
-include_dirs=[numpy.get_include()],
-ext_modules=[Extension('focuspoint.fib4', ['focuspoint/fib4.c'])]
-	)
